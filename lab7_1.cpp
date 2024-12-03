c878f1817b417bdd20901c5ef235a8ed25d11713
svre-fm <firmoioi96@gmail.com>
2024-12-03 15:10:59 +0700

    complete lab7_1

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    int a  =  20, b = 30, c = 4;
+    double output;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output = a - double(b)/c;
+    cout << "output: " <<  output << "\n";
     return 0;
 }
